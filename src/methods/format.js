3cd0d68800d12b82fba7c021b9b0aaf4e59fc370
Tami1118 <chenziyi93326@gmail.com>
2023-05-01 13:51:49 +0800

    調整參數

--- src/methods/format.js
+++ src/methods/format.js
@@ -1,7 +1,13 @@
-export const priceFormat = (price) => {
+function priceFormat(price) {
   if (price) {
     return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
   } else {
     return ''
   }
 }
+
+function dateFormat(time) {
+  return new Date(time * 1000).toLocaleDateString()
+}
+
+export { priceFormat, dateFormat }
