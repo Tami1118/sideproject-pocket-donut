6a16b0bfab6b508b9bd81df1d178a3a80043c546
Tami1118 <chenziyi93326@gmail.com>
2023-05-01 13:50:57 +0800

    調整參數

--- src/components/admin/DeleteModal.vue
+++ src/components/admin/DeleteModal.vue
@@ -2,14 +2,16 @@
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header bg-primary text-white">
-          <h5 class="modal-title">確定刪除 {{tempItem.id? '':'全部'}} ?</h5>
+          <h5 class="modal-title">
+            <span v-if="type==='訂單'">確定要刪除{{tempItem.id? '':'全部'}}</span>
+            <span v-else>確定刪除 {{tempItem.title }}</span> ?
+          </h5>
           <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          刪除後的{{type}}將無法復原，確定要刪除
-          <span v-if="tempItem.id">{{type}}編號 <strong class="text-primary fw-bold">{{tempItem.id}}</strong></span>
-          <span v-else class="text-primary fw-bold">全部</span>
-          {{type}}?
+          刪除後的{{type}}將無法復原，確定要刪除 
+          <span v-if="type==='訂單'" class="text-primary fw-bold">{{tempItem.id? `${tempItem.id}`:'全部'}}</span>
+          <span v-else class="text-primary fw-bold">{{ tempItem.title }}</span> {{type}}?
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">取消</button>
